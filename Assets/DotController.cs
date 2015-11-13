96e7ba9241ecb8d1ed5d7df10d3cdaa7b9002d9e
Matt Shapiro <matthew.shapiro@pearson.com>
2015-11-13 00:39:19 -0700

    good god wtf is with this library directory???

---
+++ Assets/DotController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy (gameObject);
+		}
+	}
+}
