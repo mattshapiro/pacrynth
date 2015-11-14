9ca90fe23f21f402d16ac36fe7a0fbf35671b2f9
Matt Shapiro <matthew.shapiro@pearson.com>
2015-11-13 21:35:46 -0700

    power pellets, fright mode, dead mode, etc

---
+++ Assets/PelletController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PelletController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			GameObject [] ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+			for(int i = 0; i < ghosts.Length; i++ )
+			{
+				GhostController ghost = (GhostController)ghosts[i].GetComponent<GhostController>();
+				ghost.BeginFrightMode ();
+			}
+			Destroy (gameObject);
+		}
+	}
+
+}
--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 public class GhostController : MonoBehaviour {
 
 	public float speed = 3.0f;
+	public float fright_time = 10;
 	public Rigidbody player;
 	public Rigidbody board;
+	public Material fright_material, dead_material;
 
 	private PathNode currentNode, lastNode;
-	private bool stopped;
+	private bool stopped, fright_mode, dead_mode;
+	private float fright_count;
+	private Material live_material;
+
+	private Renderer renderer;
 
 	public void Reset() {
 		stopped = false;
+		fright_mode = false;
 		currentNode = (PathNode)GameObject.FindGameObjectsWithTag ("PrimeNode")[0].GetComponent<PathNode>();
 		lastNode = (PathNode)GameObject.FindGameObjectsWithTag ("Respawn") [0].GetComponent<PathNode> ();
 		transform.position = lastNode.GetPosition ();
@@ -22,16 +29,36 @@ public class GhostController : MonoBehaviour {
 		stopped = true;
 	}
 
+	public void BeginFrightMode() {
+		fright_count = 0;
+		fright_mode = true;
+		dead_mode = false;
+		renderer.material = fright_material;
+	}
+
 	// Use this for initialization
 	void Start () {
+		renderer = GetComponent <Renderer> ();
+		live_material = renderer.material;
 		Reset ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (!stopped) {
+			if(fright_mode) {
+				fright_count += Time.deltaTime;
+				if(fright_count >= fright_time) {
+					fright_mode = false;
+					renderer.material = live_material;
+				}
+			}
 			Vector3 delta = currentNode.GetPosition () - transform.position;
 			if (delta.magnitude < currentNode.GetRadius ()) {
+				if(dead_mode) {
+					dead_mode = false;
+					renderer.material = live_material;
+				}
 				int nextNode = 0; 
 				do {
 					nextNode = Random.Range (0, currentNode.children.Length);
@@ -51,11 +78,21 @@ public class GhostController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag ("Player")) {
-			Stop ();
-			player.angularVelocity = new Vector3 (0f, 0f, 0f);
-			player.velocity = new Vector3 (0f, 0f, 0f);
-			board.transform.rotation = Quaternion.Euler (0f, 0f, 0f);
-			other.isTrigger = true;
+			if(fright_mode)
+			{
+				// collision with "blue ghost"
+				dead_mode = true;
+				fright_mode = false;
+				renderer.material = dead_material;
+				currentNode = (PathNode)GameObject.FindGameObjectsWithTag ("Respawn") [0].GetComponent<PathNode> ();
+			} else {
+				// falls through hole
+				Stop ();
+				player.angularVelocity = new Vector3 (0f, 0f, 0f);
+				player.velocity = new Vector3 (0f, 0f, 0f);
+				board.transform.rotation = Quaternion.Euler (0f, 0f, 0f);
+				other.isTrigger = true;
+			}
 		}
 	}
 }
