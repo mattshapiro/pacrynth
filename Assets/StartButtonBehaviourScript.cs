d49e454e83009daf24541b3add17ebafcdda8682
mattshapiro <typorrhea@gmail.com>
2016-03-03 19:12:19 -0700

    ok for real - finished flow

--- Assets/StartButtonBehaviourScript.cs
+++ Assets/StartButtonBehaviourScript.cs
@@ -6,4 +6,9 @@ public class StartButtonBehaviourScript : MonoBehaviour {
 	public void onClick() {
 		SceneManager.LoadScene("Level1");
 	}
+
+	// ug this could have gone better
+	public void onContinue() {
+		SceneManager.LoadScene ("Menu");
+	}
 }
