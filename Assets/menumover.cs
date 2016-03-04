834103171c2f860c9ce283ee6f63f694a6fd2ff7
mattshapiro <typorrhea@gmail.com>
2016-03-03 18:23:49 -0700

    paralax menu woot woot

---
+++ Assets/menumover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menumover : MonoBehaviour {
+
+	Quaternion initialPos;
+
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float former = transform.rotation.eulerAngles.y;
+		float newgle = (former + (5.0f * Time.deltaTime)) % 360;
+		transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x,
+			newgle,//transform.rotation.eulerAngles.y,
+			transform.rotation.eulerAngles.z);
+	}
+}
