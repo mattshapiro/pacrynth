01f77c8efce63b525411c19da9f702d421722eed
Matthew Shapiro <matthew.shapiro@pearson.com>
2017-06-25 14:10:56 -0500

    merging audio and scripts from VR branch

---
+++ Assets/Scripts/BallAudio.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAudio : MonoBehaviour {
+	public AudioClip loop;
+    public AudioSource source;
+    public Rigidbody ball;
+
+    bool isRolling;
+
+	// Use this for initialization
+	void Start () {
+        isRolling = false;
+        source.clip = loop;
+        source.loop = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float mag = Mathf.Min( Mathf.Abs(ball.velocity.x) + Mathf.Abs(ball.velocity.y), 5);
+        float vol = mag / 5;
+        if (isRolling)
+        {
+            source.volume = Mathf.Min(vol, 1);
+            if(mag < 0.01)
+            {
+                isRolling = false;
+                source.Stop();
+            }
+        }
+        else
+        {
+            if(mag > 0.01)
+            {
+                isRolling = true;
+                source.volume = Mathf.Min(vol, 1);
+                source.Play();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/wallcollision_audio.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallcollision_audio : MonoBehaviour {
+
+    public AudioClip clip;
+    public AudioSource source;
+
+    private void Start()
+    {
+        source.clip = clip;
+        source.loop = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject go = collision.collider.gameObject;
+        if (go.CompareTag("Player"))
+        {
+            Rigidbody body = go.GetComponent<Rigidbody>();
+            float mag = Mathf.Min(Mathf.Abs(body.velocity.x) + Mathf.Abs(body.velocity.y), 3);
+            float vol = mag / 3;
+            source.volume = Mathf.Min(vol, 1);
+            source.PlayOneShot(clip, (float)1.0);
+        }
+    }
+}
